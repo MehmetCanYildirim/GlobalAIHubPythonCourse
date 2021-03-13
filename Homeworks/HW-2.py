a321f91f7f7f95fbfa0f791008bb17a9ed66a5aa
MehmetCanYildirim <80276063+MehmetCanYildirim@users.noreply.github.com>
2021-03-13 21:59:29 +0300

    Add files via upload

---
+++ Homeworks/HW-2.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 11 22:21:34 2021
+
+@author: Mehmet Can Yıldırım
+"""
+
+
+CV1 = {"ad":"Mehmet Can", "soyad":"Yıldırım","yas":"24", "job":"Biomedical Engineer", "location":"Isparta"} # Creation of dictionary for CV1 and its informations.
+CV2 = {"ad":"Beste", "soyad":"İmamoglu","yas":"25", "job":"Biomedical Engineer", "location":"Gebze"} # Creation of dictionary for CV2 and its informations.
+CV3 = {"ad":"Ahmet", "soyad":"Yılmaz","yas":"28", "job":"Pilot", "location":"Corum"} # Creation of dictionary for CV3 and its informations.
+CV4 = {"ad":"Pelin", "soyad":"Aktürk","yas":"55", "job":"Software Engineer", "location":"İzmir"} # Creation of dictionary for CV4 and its informations.
+CV5 = {"ad":"Alex", "soyad":"De Souza","yas":"43", "job":"Footballer-Doctor", "location":"Curitiba"} # Creation of dictionary for CV5 and its informations.
+
+    
+print(CV1.values()) # Printing of CV1 that has information about the person.
+print(CV2.values()) # Printing of CV2 that has information about the person.
+print(CV3.values()) # Printing of CV3 that has information about the person.
+print(CV4.values()) # Printing of CV4 that has information about the person.
+print(CV5.values()) # Printing of CV5 that has information about the person. 
